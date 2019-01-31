4a536fe13dbdea693774f94e9351510f83bb34ce
liuzhaowei <liuzhaowei55@gmail.com>
2019-01-31 17:02:04 +0800

    配置 cors

--- Spring Boot/src/main/java/com/example/api/config/WebMvcConfigurerImpl.java
+++ Spring Boot/src/main/java/com/example/api/config/WebMvcConfigurerImpl.java
@@ -5,9 +5,11 @@ import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
+
 /**
  * WebMvcConfigurerImpl
  *
@@ -16,6 +18,11 @@ import java.util.List;
  */
 @Configuration
 public class WebMvcConfigurerImpl implements WebMvcConfigurer {
+    /**
+     * 配置 FastJson
+     *
+     * @param converters
+     */
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
@@ -25,5 +32,20 @@ public class WebMvcConfigurerImpl implements WebMvcConfigurer {
         );
         converter.setFastJsonConfig(config);
         converters.add(0, converter);
+    }
+
+    /**
+     * 配置跨域请求
+     *
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders(("*"))
+                .allowCredentials(true)
+                .maxAge(3600L);
     }
 }
