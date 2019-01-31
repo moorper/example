7b90274035a22f708bfe5aff87b939f3a7345a43
liuzhaowei <liuzhaowei55@gmail.com>
2019-01-31 15:16:28 +0800

    Spring Boot

---
+++ Spring Boot/src/main/java/com/example/api/Application.java
@@ -0,0 +1,17 @@
+package com.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application
+ * 
+ * @author Where
+ * @date 2019-01-31
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
