0d5505ea82959c20ba0fca660c6bc52d855f6d57
liuzhaowei <liuzhaowei55@gmail.com>
2019-09-17 18:33:46 +0800

    maven

---
+++ maven/src/main/java/com/moorper/example/maven/Application.java
@@ -0,0 +1,10 @@
+package com.moorper.example.maven;
+
+/**
+ * @author Where
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+    }
+}
